9900361d9d4aa8d452fd4904ea56711e08cbf5f9
kosel <koselevicila@gmail.com>
2023-04-04 16:12:14 +0300

    refactor-app-selectIsInitialized

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { useFormik } from 'formik'
-import { useSelector } from 'react-redux'
-import { loginTC } from './auth-reducer'
-import { AppRootStateType } from '../../app/store'
-import { Navigate } from 'react-router-dom'
-import { useAppDispatch } from '../../hooks/useAppDispatch';
-import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from '@mui/material'
+import {useFormik} from 'formik'
+import {useSelector} from 'react-redux'
+import {loginTC} from './auth-reducer'
+import {Navigate} from 'react-router-dom'
+import {useAppDispatch} from 'hooks/useAppDispatch';
+import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField} from '@mui/material'
+import {selectIsLoggedIn} from "features/auth/auth.selectors";
 
 export const Login = () => {
     const dispatch = useAppDispatch()
 
-    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
 
     const formik = useFormik({
         validate: (values) => {
@@ -37,7 +37,7 @@ export const Login = () => {
     })
 
     if (isLoggedIn) {
-        return <Navigate to={"/"} />
+        return <Navigate to={"/"}/>
     }
 
 
