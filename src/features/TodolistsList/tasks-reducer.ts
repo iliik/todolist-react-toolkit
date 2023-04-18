ca786af615fbf1d6590e5770e41c06f522d0ddb1
kosel <koselevicila@gmail.com>
2023-04-18 15:39:35 +0300

    tasks-reducer-createAppAsyncThunk

--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -55,17 +55,19 @@ const slice = createSlice({
     }
 })
 
-const fetchTask = createAsyncThunk('tasks/fetchTasks', async (todolistId: string, thunkAPI) => {
+const fetchTask = createAsyncThunk<{ tasks: TaskType[], todolistId: string }, string, { rejectValue: null  }>
+('tasks/fetchTasks', async (todolistId, thunkAPI) => {
     const {dispatch, rejectWithValue} = thunkAPI
-    try{
+    try {
         dispatch(appActions.setAppStatus({status: 'loading'}))
         const res = await todolistsAPI.getTasks(todolistId)
         const tasks = res.data.items
         dispatch(appActions.setAppStatus({status: 'succeeded'}))
         return ({tasks, todolistId})
-    } catch(error:any){
-        handleServerNetworkError(error,dispatch)
-    return   rejectWithValue(null)}
+    } catch (error: any) {
+        handleServerNetworkError(error, dispatch)
+        return rejectWithValue(null)
+    }
 
 })
 
